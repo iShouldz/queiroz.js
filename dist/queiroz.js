33446254791cdbbcc5536f3ba02e553339268ea9
Pedro Souza <83237899+iShouldz@users.noreply.github.com>
2024-11-07 17:15:48 -0300

    Update queiroz.js

--- dist/queiroz.js
+++ dist/queiroz.js
@@ -1339,10 +1339,10 @@
                     }
 
                     let times = [
-                        `09:${(00 + r1).padStart(2)}`,
+                        `09:${(05 + r1).padStart(2)}`,
                         `${h}:${m.padStart(2)}`,
                         `13:${r3.padStart(2)}`,
-                        `18:${00 + r1 - r2 + r3}`
+                        `18:${05 + r1 - r2 + r3}`
                     ];
 
                     eDay.querySelectorAll('.LastSlot'); // last box to insert time (only for first time)
